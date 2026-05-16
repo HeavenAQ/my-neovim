35e5bff87bddb57461de2980ddba1c3e6b22c7ac
Chen Yi Min <891118heaven@gmail.com>
2026-05-16 13:59:15 +0900

    fix: enable pyrefly return type inlay hints

--- nvim/lua/plugins/python_inlay.lua
+++ nvim/lua/plugins/python_inlay.lua
@@ -6,18 +6,25 @@ return {
       opts = opts or {}
       opts.servers = opts.servers or {}
 
+      local inlay_hints = {
+        callArgumentNames = "all",
+        functionReturnTypes = true,
+        pytestParameters = false,
+        variableTypes = true,
+      }
+
       opts.servers.pyrefly = vim.tbl_deep_extend("force", opts.servers.pyrefly or {}, {
         init_options = {
+          python = {
+            analysis = {
+              inlayHints = inlay_hints,
+            },
+          },
           pyrefly = {
             typeCheckingMode = "auto",
             analysis = {
               diagnosticMode = "openFilesOnly",
-              inlayHints = {
-                callArgumentNames = "all",
-                functionReturnTypes = true,
-                pytestParameters = false,
-                variableTypes = true,
-              },
+              inlayHints = inlay_hints,
             },
           },
         },
